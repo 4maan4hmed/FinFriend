e2ca886fec37a385ab91fd5cfa9145f6ed5c1430
Amaan Ahmad <amaan.ahmad2022@vitstudent.ac.in>
2025-04-04 11:20:42 +0530

    baba

--- fintech_app/lib/widgets/finance/daily_change_card.dart
+++ fintech_app/lib/widgets/finance/daily_change_card.dart
@@ -19,7 +19,7 @@ class DailyChangeCard extends StatelessWidget {
 
     return Container(
       padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
+      decoration: BoxDecoration(gi
         color: Colors.white,
         borderRadius: BorderRadius.circular(12),
         boxShadow: [
